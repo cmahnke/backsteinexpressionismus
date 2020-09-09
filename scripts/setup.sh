2e1507aa035d0290e61e104430154168f7aff682
Christian Mahnke <cmahnke@gmail.com>
2020-09-09 05:00:05 +0200

    More infrastructure

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 WD=`pwd`
 
 # IIFF
-for IMAGE in `ls -1 content/post/**/page*.jpg content/post/**/front.jpg`
+for IMAGE in `ls -1 content/post/**/page*.jpg content/post/**/front.jpg content/post/**/end.jpg`
 do
     echo "Generating IIIF files for $IMAGE"
 #    iiif_static.py -d `dirname $IMAGE` $IMAGE
