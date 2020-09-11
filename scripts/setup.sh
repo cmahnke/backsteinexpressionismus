ce049838b2da669261f268239f4b53aaa5663ee7
Christian Mahnke <cmahnke@gmail.com>
2020-09-11 17:31:35 +0200

    Added faster image tiler

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-WD=`pwd`
-
-# IIFF
-for IMAGE in `ls -1 content/post/**/page*.jpg content/post/**/front.jpg content/post/**/end.jpg`
-do
-    echo "Generating IIIF files for $IMAGE"
-#    iiif_static.py -d `dirname $IMAGE` $IMAGE
-done
+./scripts/iiif.sh
