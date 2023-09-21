a92b1f59ab3db7b60a3b53681f46f091b0586f81
Christian Mahnke <cmahnke@gmail.com>
2023-09-21 13:05:30 +0200

    Fix bbox build

--- scripts/map.sh
+++ scripts/map.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-hugo --disableKinds=pages,taxonomy,taxonomyTerm,category,sitemap,RSS,404,robotsTXT,home
+hugo --disableKinds=pages,category,sitemap,RSS,404,robotsTXT,home
 python themes/projektemacher-base/scripts/bbox.py -f docs/tags/map.geojson -j -m 100000 > assets/js/bbox.json
