eaec17ff84a4fa2dfe0f7943b6a80d7e5408f65c
Christian Mahnke <cmahnke@gmail.com>
2023-09-21 10:57:38 +0200

    Prepare map

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,3 +25,5 @@ TARGETFORMAT=png ./themes/projektemacher-base/scripts/preview.sh
 SOURCE="Source Files/Favicon/Favicon.psd[1]" OPTIONS="-background 'rgba(255, 255, 255, .0)' -resize 300x300 -gravity center -extent 300x300 " ./themes/projektemacher-base/scripts/favicon.sh
 
 yarn run svgo
+
+./scripts/map.sh
